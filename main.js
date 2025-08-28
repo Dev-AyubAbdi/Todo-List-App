ae46d2fad7b7c5f2e2237de2a21bbfc43e77a76f
Dev-AyubAbdi <ayubabdiibrahiim@gmail.com>
2025-08-28 16:34:59 +0300

    Added line js

--- main.js
+++ main.js
@@ -1,47 +1,58 @@
-const TodoForm = document.querySelector('#todo-Form');
-const TodoInput = document.querySelector('#Todo-Input');
-const todoList = document.querySelector('.Todo-Lists');
+const TodoForm = document.querySelector("#todo-Form");
+const TodoInput = document.querySelector("#Todo-Input");
+const todoList = document.querySelector(".Todo-Lists");
 
-TodoForm.addEventListener('submit', addTaks)
+TodoForm.addEventListener("submit", addTaks);
 
-function addTaks(event) {
-    event.preventDefault()
+document.addEventListener('DOMContentLoaded', loadTask);
 
-    const tasks = TodoInput.value.trim()
+function loadTask() {
+    const tasks = JSON.parse(localStorage.getItem('tasks')) || []
 
-    if(tasks !== " ") {
-        const task = {
-            Id: Date.now(),
-            text: tasks,
-            completed: false
-
-        }
-
+    tasks.forEach(task => {
         addToDom(task)
-        TodoInput.value = ""
-        addTolocalStorage(task)
-    }
+    })
+}
+ 
+function addTaks(event) {
+  event.preventDefault();
+
+  const tasks = TodoInput.value.trim();
+
+  if (tasks !== " ") {
+    const task = {
+      Id: Date.now(),
+      text: tasks,
+      completed: false,
+    };
+
+    addToDom(task);
+    TodoInput.value = "";
+    addTolocalStorage(task);
+  }
 }
 
 function addToDom(task) {
-    const li = document.createElement('li')
-    li.className = "todo-Items"
-    li.dataset.id = task.id
+  const li = document.createElement("li");
+  li.className = "todo-Items";
+  li.dataset.id = task.id;
 
-    li.innerHTML = 
-         `   <input type="checkbox" class="checkbox">
+  li.innerHTML = `   <input type="checkbox" class="checkbox">
                 <span class="task">${task.text}</span>
                 <button class="Edite-Btn">Edite</button>
                 <button class="Delete-Btn">Delete</button>
-            `
+            `;
 
-      todoList.appendChild(li)
-
-
+  todoList.appendChild(li);
 }
 
 function addTolocalStorage(task) {
 
+    const tasks = JSON.parse(localStorage.getItem('tasks')) || []
 
-    localStorage.setItem('tasks', JSON.stringify(task))
+    tasks.push(task);
+    
+    localStorage.setItem('tasks', JSON.stringify(tasks))
 }
+
+
